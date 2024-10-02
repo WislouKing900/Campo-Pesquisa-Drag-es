dce3b780f534c63fe9524ccb14600eb9b5bdea42
lucas machado <machado.lucas.900@gmail.com>
2024-10-01 22:27:31 -0300

    dragões no js

--- dados.js
+++ dados.js
@@ -2,7 +2,7 @@ let dados = [
     {
         titulo: 'Dragon',
         descricao: 'A palavra "dragon" tem raízes indo-européias e é a desiginação mais comum para essas criaturas em muitas culturas ocidentais. Geralmente descritos como grandes répteis alados, com escamas, garras afiadas e a capacidade de cuspir fogo. Sua aparência e poderes variam muito dependendo da mitologia. Representam poder, força, sabedoria e, em algumas culturas, até mesmo a criação.', 
-        tags: 'fogo dragão'
+        tags: 'fogo dragão dragões'
     },
     {
         titulo: 'Drake',
@@ -12,12 +12,12 @@ let dados = [
     {
         titulo: 'Wyvern',
         descricao: 'O wyvern é uma criatura semelhante a um dragão, mas com apenas duas patas e uma cauda que termina em um ferrão venenoso. Eles são frequentemente retratados como mais ágeis e rápidos que os dragões, e são conhecidos por sua natureza agressiva.',
-        tags: 'galinha'
+        tags: 'galinha dragões ferrão drogon'
     },
     {
         titulo: 'Fae',
         descricao: 'Dragões Fae são criaturas míticas que surgem da fusão entre a mitologia dos dragões e a do povo Fae, ou seja, as fadas. Essa combinação resulta em seres fascinantes, com características únicas que os diferenciam tanto dos dragões tradicionais quanto das fadas clássicas.A aparência dos dragões Fae pode variar muito, dependendo da tradição e da história. No entanto, algumas características comuns',
-        tags: 'dragãozinho'
+        tags: 'dragãozinho dragões'
     }
     
 ];
